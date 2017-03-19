c14b330cff746c68ca3219b994ba84e5373ee5fa
Peter Vu <me@petervu.ca>
2017-03-19 00:16:30 -0400

    Actual initial setup

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,66 +1,188 @@
-import {
-  GoogleMap,
-  GoogleMapsEvent,
-  GoogleMapsLatLng,
-  CameraPosition,
-  GoogleMapsMarkerOptions,
-  GoogleMapsMarker,
-  GoogleMapsMapTypeId
-} from 'ionic-native';
-
-import { Component } from '@angular/core';
-
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+ 
+declare var google;
+ 
 @Component({
-  selector: 'page-home',
+  selector: 'home-page',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+ 
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+ 
   constructor(public navCtrl: NavController) {
-
+ 
   }
-
-  ngAfterViewInit() {
+ 
+  ionViewDidLoad(){
     this.loadMap();
   }
+ 
+  loadMap(){
+ 
+    let latLng = new google.maps.LatLng(-34.9290, 138.6010);
+ 
+    let mapOptions = {
+      center: latLng,
+      zoom: 15,
+      disableDefaultUI: true,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      styles:[
+    {
+        "featureType": "administrative",
+        "elementType": "all",
+        "stylers": [
+            {
+                "saturation": "-100"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative.province",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "all",
+        "stylers": [
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 65
+            },
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "all",
+        "stylers": [
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": "50"
+            },
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "all",
+        "stylers": [
+            {
+                "saturation": "-100"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "all",
+        "stylers": [
+            {
+                "lightness": "30"
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "all",
+        "stylers": [
+            {
+                "lightness": "40"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "all",
+        "stylers": [
+            {
+                "saturation": -100
+            },
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "hue": "#ffff00"
+            },
+            {
+                "lightness": -25
+            },
+            {
+                "saturation": -97
+            } 
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "lightness": -25
+            },
+            {
+                "saturation": -100
+            }
+        ]
+    }
+]
+    }
+ 
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+ 
+  }
 
-  loadMap() {
+  addMarker(){
+ 
+    let marker = new google.maps.Marker({
+      map: this.map,
+      animation: google.maps.Animation.DROP,
+      position: this.map.getCenter()
+    });
+  
+    let content = "<h4>Information!kkhlkjhlkjhlkjhlkjhlkjhlkjhlkjhlkjhlkjhlkjhlkjhlkj</h4>";          
+  
+    this.addInfoWindow(marker, content);
+  
+  }
 
-    // create a new map by passing HTMLElement
-    let element: HTMLElement = document.getElementById('map');
-
-    let map = new GoogleMap(element);
-
-    // create LatLng object
-    let ionic: GoogleMapsLatLng = new GoogleMapsLatLng(43.0741904, -89.3809802);
-
-    // create CameraPosition
-    let position: CameraPosition = {
-      target: ionic,
-      zoom: 18,
-      tilt: 30
-    };
-
-    // listen to MAP_READY event
-    map.one(GoogleMapsEvent.MAP_READY).then(() => {
-      // move the map's camera to position
-      map.moveCamera(position); // works on iOS and Android
-    })
-
-
-    // create new marker
-    let markerOptions: GoogleMapsMarkerOptions = {
-      position: ionic,
-      title: 'Ionic'
-    };
-
-    map.addMarker(markerOptions)
-      .then((marker: GoogleMapsMarker) => {
-        marker.showInfoWindow();
-      });
-
-
+  addInfoWindow(marker, content){
+ 
+    let infoWindow = new google.maps.InfoWindow({
+      content: content
+    });
+  
+    google.maps.event.addListener(marker, 'click', () => {
+      infoWindow.open(this.map, marker);
+    });
+  
   }
 }
